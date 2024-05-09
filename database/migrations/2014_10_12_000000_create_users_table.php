bf3faf7b97f5379e2470126556515cd8d8d8a3e1
nendraJzz <nendrashidiq123@gmail.com>
2024-05-09 16:58:55 +0700

    addisiopsi

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('level');
-            $table->string('nohp');
+            $table->integer('nohp')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
